77bb317f32281dc746af12086ad556c8ecd9b504
Jens <jens.jens@live.dk>
2024-01-30 16:01:49 +0100

    chore: converted to workspaces

--- gbpplanner-rs/src/main.rs
+++ gbpplanner-rs/src/main.rs
@@ -1,10 +1,8 @@
 mod config;
-mod factor;
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
-
-use clap::{Parser, Subcommand};
-
 use crate::config::Config;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use clap::{Parser, Subcommand};
+// use gbp_rs::
 
 #[derive(Parser)]
 #[clap(version = "0.1.0", author = "Johannes Schickling")]
@@ -33,93 +31,6 @@ struct Cli {
 //         next_frame().await
 //     }
 // }
-
-// struct RobotId(usize);
-type RobotId = usize;
-// struct NodeId(usize);
-type NodeId = usize;
-
-trait Factor {}
-
-#[derive(Debug)]
-struct DefaultFactor;
-#[derive(Debug)]
-struct DynamicFactor;
-#[derive(Debug)]
-struct InterRobotFactor;
-#[derive(Debug)]
-struct ObstacleFactor;
-
-impl Factor for DefaultFactor {}
-impl Factor for DynamicFactor {}
-impl Factor for InterRobotFactor {}
-impl Factor for ObstacleFactor {}
-
-#[derive(Debug)]
-enum FactorType {
-    Default(DefaultFactor),
-    Dynamic(DynamicFactor),
-    InterRobot(InterRobotFactor),
-    Ocstacle(ObstacleFactor),
-}
-
-// enum MsgPassingMode {EXTERNAL, INTERNAL};
-
-/// Ways message passing can be performed
-#[derive(Debug)]
-enum MessagePassingMode {
-    /// Between two different robots/factorgraphs
-    External,
-    /// Within a robot/factorgraph
-    Internal,
-}
-// Eigen::VectorXd eta;
-// Eigen::MatrixXd lambda;
-// Eigen::VectorXd mu;
-
-#[derive(Debug)]
-struct Message {
-    pub eta: nalgebra::DVector<f64>,
-    pub lambda: nalgebra::DMatrix<f64>,
-    pub mu: nalgebra::DVector<f64>,
-}
-
-// Implement addtion and subtraction for messages
-impl std::ops::Add for Message {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            eta: self.eta + other.eta,
-            lambda: self.lambda + other.lambda,
-            mu: self.mu + other.mu,
-        }
-    }
-}
-
-impl std::ops::Sub for Message {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-        Self {
-            eta: self.eta - other.eta,
-            lambda: self.lambda - other.lambda,
-            mu: self.mu - other.mu,
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd)]
-struct Variable {
-    node_id: NodeId,
-    robot_id: RobotId,
-}
-
-impl Variable {
-    fn new(node_id: NodeId, robot_id: RobotId) -> Self {
-        Self { node_id, robot_id }
-    }
-}
 
 fn hello_world() {
     println!("hello world!");
