7b231bc991ffd9358f0f9259ff85b9ef773a1030
Jens <jens.jens@live.dk>
2024-02-08 15:53:03 +0100

    feat: toglling, moving, boosting all hooked to input on keyboard and gamepad

--- gbpplanner-rs/src/input.rs
+++ gbpplanner-rs/src/input.rs
@@ -1,4 +1,5 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, transform};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+// use bevy_inspector_egui::egui::scroll_area::State;
 use leafwing_input_manager::{prelude::*, user_input::InputKind};
 
 pub struct InputPlugin;
@@ -9,14 +10,33 @@ pub struct InputPlugin;
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(InputManagerPlugin::<InputAction>::default())
+            .add_state::<MoveableObjectMovementState>()
+            .add_state::<MoveableObjectVisibilityState>()
             .add_systems(Startup, bind_input)
-            .add_systems(Update, use_actions);
+            .add_systems(Update, (visibility_actions, movement_actions));
     }
+}
+
+/// Here, we define a State for Scenario.
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum MoveableObjectMovementState {
+    #[default]
+    Default,
+    Boost,
+}
+
+// define visibility state for the moveable object
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum MoveableObjectVisibilityState {
+    #[default]
+    Visible,
+    Hidden,
 }
 
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
 enum InputAction {
-    Move,
+    MoveObject,
+    MoveCamera,
     Boost,
     Toggle,
 }
@@ -26,7 +46,8 @@ impl InputAction {
     fn default_keyboard_mouse_input(action: InputAction) -> UserInput {
         // Match against the provided action to get the correct default keyboard-mouse input
         match action {
-            Self::Move => UserInput::VirtualDPad(VirtualDPad::wasd()),
+            Self::MoveObject => UserInput::VirtualDPad(VirtualDPad::wasd()),
+            Self::MoveCamera => UserInput::VirtualDPad(VirtualDPad::arrow_keys()),
             Self::Boost => UserInput::Single(InputKind::Keyboard(KeyCode::ShiftLeft)),
             Self::Toggle => UserInput::Single(InputKind::Keyboard(KeyCode::F)),
         }
@@ -35,7 +56,8 @@ impl InputAction {
     fn default_gamepad_input(action: InputAction) -> UserInput {
         // Match against the provided action to get the correct default gamepad input
         match action {
-            Self::Move => UserInput::Single(InputKind::DualAxis(DualAxis::left_stick())),
+            Self::MoveObject => UserInput::Single(InputKind::DualAxis(DualAxis::left_stick())),
+            Self::MoveCamera => UserInput::Single(InputKind::DualAxis(DualAxis::right_stick())),
             Self::Boost => {
                 UserInput::Single(InputKind::GamepadButton(GamepadButtonType::LeftTrigger))
             }
@@ -77,22 +99,30 @@ fn bind_input(
         .insert(MoveableObject);
 }
 
-fn use_actions(mut query: Query<(&ActionState<InputAction>, &mut Transform)>) {
+fn movement_actions(
+    mut next_state: ResMut<NextState<MoveableObjectMovementState>>,
+    state: Res<State<MoveableObjectMovementState>>,
+    mut query: Query<(&ActionState<InputAction>, &mut Transform)>,
+) {
     // let action_state = query.single();
     let (action_state, mut _moveable_object) = query.single_mut();
 
     // When the default input for `InputAction::Move` is pressed, print the clamped direction of the axis
-    if action_state.pressed(InputAction::Move) {
+    if action_state.pressed(InputAction::MoveObject) {
+        let scale = match state.get() {
+            MoveableObjectMovementState::Default => 3.0,
+            MoveableObjectMovementState::Boost => 10.0,
+        };
         _moveable_object.translation += action_state
-            .clamped_axis_pair(InputAction::Move)
+            .clamped_axis_pair(InputAction::MoveObject)
             .unwrap()
             .xy()
             .extend(0.0)
-            * 5.0;
+            * scale;
         println!(
             "Moving in direction {}",
             action_state
-                .clamped_axis_pair(InputAction::Move)
+                .clamped_axis_pair(InputAction::MoveObject)
                 .unwrap()
                 .xy()
         );
@@ -101,10 +131,52 @@ fn use_actions(mut query: Query<(&ActionState<InputAction>, &mut Transform)>) {
     // When the default input for `InputAction::Boost` is pressed, print "Using Boost!"
     if action_state.just_pressed(InputAction::Boost) {
         println!("Using Boost!");
+        match state.get() {
+            MoveableObjectMovementState::Default => {
+                next_state.set(MoveableObjectMovementState::Boost);
+            }
+            MoveableObjectMovementState::Boost => {
+                next_state.set(MoveableObjectMovementState::Default);
+            }
+        }
     }
+
+    // // When the default input for `InputAction::Toggle` is pressed, print "Toggled moveable actor!"
+    // if action_state.just_pressed(InputAction::Toggle) {
+    //     println!("Toggled moveable actor!");
+    //     // toogle the MoveableObject
+    // }
+}
+
+fn visibility_actions(
+    mut next_state: ResMut<NextState<MoveableObjectVisibilityState>>,
+    state: Res<State<MoveableObjectVisibilityState>>,
+    mut query: Query<(&ActionState<InputAction>, &Handle<ColorMaterial>)>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let (action_state, mut _handle) = query.single_mut();
 
     // When the default input for `InputAction::Toggle` is pressed, print "Toggled moveable actor!"
     if action_state.just_pressed(InputAction::Toggle) {
         println!("Toggled moveable actor!");
+        // toogle the MoveableObject
+        if let Some(material) = materials.get_mut(_handle) {
+            match state.get() {
+                MoveableObjectVisibilityState::Visible => {
+                    // hide the moveable object by setting the alpha to 0
+                    let mut color = material.color;
+                    color.set_a(0.0);
+                    material.color = color;
+                    next_state.set(MoveableObjectVisibilityState::Hidden);
+                }
+                MoveableObjectVisibilityState::Hidden => {
+                    // show the moveable object by setting the alpha to 1
+                    let mut color = material.color;
+                    color.set_a(1.0);
+                    material.color = color;
+                    next_state.set(MoveableObjectVisibilityState::Visible);
+                }
+            }
+        }
     }
 }
