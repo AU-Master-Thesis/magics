ffa2883bc3ad0c1e8ff70c520c269c583a6495e5
Jens <jens.jens@live.dk>
2024-03-07 13:48:34 +0100

    feat(visualisation): drawing trajectory lines

--- gbpplanner-rs/src/planner/visualiser.rs
+++ gbpplanner-rs/src/planner/visualiser.rs
@@ -1,8 +1,15 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    render::{mesh::PrimitiveTopology, render_asset::RenderAssetUsages},
+};
+use itertools::Itertools;
 
-use crate::asset_loader::SceneAssets;
+use crate::{
+    asset_loader::SceneAssets,
+    theme::{CatppuccinTheme, ColorFromCatppuccinColourExt},
+};
 
-use super::{robot::Waypoints, RobotId};
+use super::{factor::Factor, factorgraph, robot::Waypoints, RobotId};
 
 /// A **Bevy** `Plugin` for visualising aspects of the planner
 /// Includes visualising parts of the factor graph
@@ -16,7 +23,8 @@ impl Plugin for VisualiserPlugin {
                 init_waypoints,
                 update_waypoints,
                 init_factorgraphs,
-                update_factorgraphs,
+                // update_factorgraphs,
+                draw_lines,
             ),
         );
     }
@@ -28,6 +36,7 @@ impl Plugin for VisualiserPlugin {
 pub struct RobotTracker {
     pub robot_id: RobotId,
     pub variable_id: usize,
+    pub order: usize,
 }
 
 impl RobotTracker {
@@ -35,11 +44,17 @@ impl RobotTracker {
         Self {
             robot_id,
             variable_id: 0,
+            order: 0,
         }
     }
 
     pub fn with_variable_id(mut self, id: usize) -> Self {
         self.variable_id = id;
+        self
+    }
+
+    pub fn with_order(mut self, order: usize) -> Self {
+        self.order = order;
         self
     }
 }
@@ -50,7 +65,7 @@ pub struct WaypointVisualiser;
 
 /// A **Bevy** `Component` to mark an entity as a visualised factor graph
 #[derive(Component)]
-pub struct FactorGraphVisualiser;
+pub struct VariableVisualiser;
 
 /// A **Bevy** `Component` to mark a robot that it has a corresponding `WaypointVis` entity
 /// Useful for easy exclusion in queries
@@ -61,6 +76,91 @@ pub struct HasWaypointVisualiser;
 /// Useful for easy exclusion in queries
 #[derive(Component)]
 pub struct HasFactorGraphVisualiser;
+
+/// A marker component for lines
+/// Generally used to identify previously spawned lines,
+/// so they can be updated or removed
+#[derive(Component, Debug)]
+pub struct Line;
+
+/// A list of vertices defining a path
+#[derive(Debug, Clone)]
+struct Path {
+    points: Vec<Vec3>,
+    width: f32,
+}
+
+impl Path {
+    pub fn new(points: Vec<Vec3>) -> Self {
+        Self { points, width: 0.1 }
+    }
+
+    pub fn with_width(mut self, width: f32) -> Self {
+        self.width = width;
+        self
+    }
+}
+
+impl From<Path> for Mesh {
+    fn from(line: Path) -> Self {
+        let vertices = line.points.clone();
+        let width = line.width;
+
+        let mut left_vertices = Vec::<Vec3>::with_capacity(vertices.len());
+        let mut right_vertices = Vec::<Vec3>::with_capacity(vertices.len());
+
+        // add the first offset
+        let (a, b) = (vertices[0], vertices[1]);
+        let ab = (b - a).normalize();
+        let n = Vec3::new(ab.z, ab.y, -ab.x);
+        let left = a - n * width / 2.0;
+        let right = a + n * width / 2.0;
+        left_vertices.push(left);
+        right_vertices.push(right);
+
+        for window in vertices.windows(3) {
+            let (a, b, c) = (window[0], window[1], window[2]);
+            let ab = (b - a).normalize();
+            let bc = (c - b).normalize();
+
+            let angle = (std::f32::consts::PI - ab.dot(bc).acos()) / 2.0;
+            let kinked_width = width / angle.sin();
+
+            let n = {
+                let sum = (ab + bc).normalize();
+                Vec3::new(sum.z, sum.y, -sum.x)
+            };
+            let left = b - n * kinked_width / 2.0;
+            let right = b + n * kinked_width / 2.0;
+
+            left_vertices.push(left);
+            right_vertices.push(right);
+        }
+
+        // add the last offset
+        let (a, b) = (vertices[vertices.len() - 2], vertices[vertices.len() - 1]);
+        let ab = (b - a).normalize();
+        let n = Vec3::new(ab.z, ab.y, -ab.x);
+        let left = b - n * width / 2.0;
+        let right = b + n * width / 2.0;
+        left_vertices.push(left);
+        right_vertices.push(right);
+
+        // collect all vertices
+        let vertices: Vec<Vec3> = left_vertices
+            .iter()
+            .zip(right_vertices.iter())
+            .flat_map(|(l, r)| [*r, *l])
+            .collect();
+
+        Mesh::new(
+            PrimitiveTopology::TriangleStrip,
+            RenderAssetUsages::MAIN_WORLD  | RenderAssetUsages::RENDER_WORLD
+        )
+        // Add the vertices positions as an attribute
+        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    }
+}
 
 /// A **Bevy** `Update` system
 /// Initialises each new `FactorGraph` component to have a matching `PbrBundle` and `FactorGraphVisualiser` component
@@ -74,24 +174,29 @@ fn init_factorgraphs(
         // Mark the robot with `HasFactorGraphVisualiser` to exclude next time
         commands.entity(entity).insert(HasFactorGraphVisualiser);
 
-        factorgraph.variables().for_each(|v| {
-            let mean = v.belief.mean();
-            let transform = Vec3::new(mean[0] as f32, 0.0, mean[1] as f32);
+        factorgraph
+            .variables_ordered()
+            .enumerate()
+            .for_each(|(i, v)| {
+                let mean = v.belief.mean();
+                let transform = Vec3::new(mean[0] as f32, 0.0, mean[1] as f32);
 
-            info!("{:?}: Initialising variable at {:?}", entity, transform);
+                // info!("{:?}: Initialising variable at {:?}", entity, transform);
 
-            // Spawn a `FactorGraphVisualiser` component with a corresponding `PbrBundle`
-            commands.spawn((
-                RobotTracker::new(entity).with_variable_id(v.get_node_index().index()),
-                FactorGraphVisualiser,
-                PbrBundle {
-                    mesh: scene_assets.meshes.variable.clone(),
-                    material: scene_assets.materials.variable.clone(),
-                    transform: Transform::from_translation(transform),
-                    ..Default::default()
-                },
-            ));
-        });
+                // Spawn a `FactorGraphVisualiser` component with a corresponding `PbrBundle`
+                commands.spawn((
+                    RobotTracker::new(entity)
+                        .with_variable_id(v.get_node_index().index())
+                        .with_order(i),
+                    VariableVisualiser,
+                    PbrBundle {
+                        mesh: scene_assets.meshes.variable.clone(),
+                        material: scene_assets.materials.variable.clone(),
+                        transform: Transform::from_translation(transform),
+                        ..Default::default()
+                    },
+                ));
+            });
     }
 }
 
@@ -101,7 +206,7 @@ fn init_factorgraphs(
 /// that have matching `Entity` with the `RobotTracker.robot_id`
 /// and variables in the `FactorGraph` that have matching `RobotTracker.variable_id`
 fn update_factorgraphs(
-    mut tracker_query: Query<(&RobotTracker, &mut Transform), With<FactorGraphVisualiser>>,
+    mut tracker_query: Query<(&RobotTracker, &mut Transform), With<VariableVisualiser>>,
     factorgraph_query: Query<(Entity, &super::FactorGraph)>,
 ) {
     // Update the `RobotTracker` components
@@ -126,6 +231,60 @@ fn update_factorgraphs(
                 transform.translation = Vec3::new(mean[0] as f32, 0.0, mean[1] as f32);
             }
         }
+    }
+}
+
+/// A **Bevy** `Update` system
+/// Draws lines between all variables in each factor graph
+///
+/// Despawns old lines, and spawns new lines
+///
+/// Queries variables by `RobotTracker` with the `FactorGraphVisualiser` component
+/// as initialised by the `init_factorgraphs` system
+/// -> Will return if this query is empty
+fn draw_lines(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    query_variables: Query<(&RobotTracker, &Transform), With<VariableVisualiser>>,
+    query_previous_lines: Query<Entity, With<Line>>,
+    factorgraph_query: Query<Entity, With<super::FactorGraph>>,
+    catppuccin_theme: Res<CatppuccinTheme>,
+) {
+    // If there are no variables visualised yet by the `init_factorgraphs` system, return
+    if query_variables.iter().count() == 0 {
+        return;
+    }
+
+    // Remove previous lines
+    // TODO: Update lines instead of removing and re-adding
+    for entity in query_previous_lines.iter() {
+        commands.entity(entity).despawn();
+    }
+
+    let line_material = materials.add(Color::from_catppuccin_colour(
+        catppuccin_theme.flavour.text(),
+    ));
+
+    for entity in factorgraph_query.iter() {
+        let positions = query_variables
+            .iter()
+            .filter(|(tracker, _)| tracker.robot_id == entity)
+            .sorted_by(|(a, _), (b, _)| a.order.cmp(&b.order))
+            .rev()
+            .map(|(_, t)| t.translation)
+            .collect::<Vec<Vec3>>();
+
+        let line = Path::new(positions).with_width(0.2);
+
+        commands.spawn((
+            PbrBundle {
+                mesh: meshes.add(Mesh::from(line)),
+                material: line_material.clone(),
+                ..Default::default()
+            },
+            Line,
+        ));
     }
 }
 
