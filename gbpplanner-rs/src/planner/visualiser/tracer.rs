e05aa164ab7d0eb6f79d6c2bd7e2c5d552fa9c5a
kpbaks <kristoffer.pbs@gmail.com>
2024-03-22 14:40:59 +0100

    feat(tracer): use a ringbuffer instead of a `Vec` to ensure an upper bound on the number of traces captured per robot

--- gbpplanner-rs/src/planner/visualiser/tracer.rs
+++ gbpplanner-rs/src/planner/visualiser/tracer.rs
@@ -1,6 +1,11 @@
 use std::collections::BTreeMap;
 
 use bevy::prelude::*;
+use itertools::Itertools;
+use ringbuf::{HeapRb, Rb, StaticRb};
+
+const MAX_TRACE_LENGTH: usize = 100;
+const SAMPLE_DELAY: f32 = 0.5;
 
 use crate::{
     config::Config,
@@ -20,14 +25,16 @@ impl Plugin for TracerVisualiserPlugin {
 // IDEA: use a ring buffer to store the traces, to ensure a maximum fixed size
 /// **Bevy** [`Resource`] to store all robot traces
 #[derive(Default, Resource)]
-pub struct Traces(pub BTreeMap<RobotId, Vec<Vec3>>);
+// pub struct Traces(pub BTreeMap<RobotId, Vec<Vec3>>);
+// pub struct Traces(pub BTreeMap<RobotId, HeapRb<Vec3>>);
+pub struct Traces(pub BTreeMap<RobotId, StaticRb<Vec3, MAX_TRACE_LENGTH>>);
 
 // #[derive(Resource)]
 pub struct SampleDelay(Timer);
 
 impl Default for SampleDelay {
     fn default() -> Self {
-        Self(Timer::from_seconds(0.5, TimerMode::Repeating))
+        Self(Timer::from_seconds(SAMPLE_DELAY, TimerMode::Repeating))
     }
 }
 
@@ -46,12 +53,14 @@ fn track_robots(
     debug!("sampling robot positions");
 
     for (robot_id, transform) in query.iter() {
-        traces
+        let _ = traces
             .0
             .entry(robot_id)
-            .or_insert_with(Vec::new)
-            .push(transform.translation);
-        // error!("transform: {:?}", transform.translation);
+            .or_insert_with(StaticRb::default)
+            // .or_insert_with(|| HeapRb::new(MAX_TRACE_LENGTH))
+            .push_overwrite(transform.translation);
+        // .or_insert_with(Vec::new)
+        // .push(transform.translation);
     }
 }
 
@@ -69,21 +78,24 @@ fn draw_traces(
     }
 
     // TODO: avoid allocating a new iterator every frame
-    let mut colours = catppuccin_theme.colours().into_iter().cycle();
-    // let colours = catppuccin_theme.colours().into_iter().collect::<Vec<_>>();
+    // let mut colours = catppuccin_theme.colours().into_iter().cycle();
+    let colours = catppuccin_theme.colours().into_iter().collect::<Vec<_>>();
 
-    for trace in traces.0.values() {
-        let color = colours.next().unwrap();
+    for (robot_id, trace) in traces.0.iter() {
+        // let color = colours.next().unwrap();
+        // PERF: compute the color once, and store it in the traces resource or a Local
+        let color = colours[robot_id.index() as usize % colours.len()];
         let color = Color::from_catppuccin_colour(color);
 
         // error!("trace: {:?}", trace);
 
         // use a window of length 2 to iterate over the trace, and draw a line between
         // each pair of points
-        for window in trace.windows(2) {
-            let start = window[0];
-            let end = window[1];
-            gizmos.line(start, end, color);
+        // for window in trace.windows(2) {
+        for (start, end) in trace.iter().tuple_windows() {
+            // let start = window[0];
+            // let end = window[1];
+            gizmos.line(*start, *end, color);
         }
 
         // let initial_position = trace.first().unwrap();
