ba817132c597e7ad71df978dee272bcbe24554ce
Jens <jens.jens@live.dk>
2024-04-25 17:04:00 +0200

    feat(env-to-png): env-to-png init function, but not usable yet

--- crates/env-to-png/src/env_to_png.rs
+++ crates/env-to-png/src/env_to_png.rs
@@ -1,7 +1,17 @@
+use std::num::NonZeroU32;
+
 use gbpplanner_rs::config::Environment;
+use image::RgbImage;
+
+/// Custom resolution type, as pixels per tile.
+pub type PixelsPerTile = NonZeroU32;
 
 /// Convert [`Environment`] to a PNG image.
-pub fn env_to_png(env: Environment) {
+pub fn env_to_png(env: Environment, resolution: PixelsPerTile) -> anyhow::Result<RgbImage> {
     let tile_size = env.tile_size();
-    env.tiles.grid.nrows()
+    let (ncols, nrows) = (env.tiles.grid.ncols(), env.tiles.grid.nrows());
+
+    let mut image = RgbImage::new(ncols as u32 * resolution.get(), nrows as u32 * resolution.get());
+
+    Ok(image)
 }
