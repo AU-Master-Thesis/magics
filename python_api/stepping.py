485fa661b77d98914cd95d09bcf01fd5bd71b90d
Zartris <jonas.le.fevre@gmail.com>
2025-03-23 22:11:44 +0100

    stepping

--- python_api/stepping.py
+++ python_api/stepping.py
@@ -6,7 +6,7 @@ client = MagicsClient()
 if not client.is_api_active():
     client.set_api_active(True)
 
-client.set_iterations_per_step(iterations=2)  # 5 iterations per step
+client.set_iterations_per_step(iterations=10)  # 5 iterations per step
 
 print("Press Enter to step the simulation (Ctrl+C to exit)...")
 
